383aff7b980a2bc2bfb58c2a063fe878aa5a2459
David Preece <davep@zedkep.com>
2025-05-12 11:30:10 +1200

    Moved from main repo

---
+++ docs/docs.js
@@ -0,0 +1,56 @@
+const thispage = window.location.pathname.split('/').pop()
+const sections = [
+    ['index.html', 'Docs'],
+    ['app.html', 'Using the App'],
+    ['importing.html', 'Manually Importing'],
+    ['web.html', 'Web Interface'],
+    ['main.html', 'Main View'],
+    ['referencing.html', 'Water Referencing'],
+    ['numbers.html', 'Numbers'],
+    ['sailing.html', 'Sailing'],
+    ['management.html', 'Management'],
+    ['technical.html', 'Technical'],
+]
+
+const existingHtml = document.getElementsByTagName('body')[0].innerHTML
+document.getElementsByTagName('body')[0].innerHTML = `
+    <div class="docs">
+        <div class="docsidebar"></div>
+        <div class="doccontent">
+            ${existingHtml}
+            <div class="bottomnav">
+                <div class="bottomnavbefore"></div>
+                <div class="bottomnavafter"></div>
+            </div>
+        </div>
+    </div>
+`
+let sectionIdx
+let idx = 0
+let ih = `<nav class="doclist">`
+for (section of sections) {
+    if (section[0] === thispage) {
+        sectionIdx = idx
+        ih += `<p><a class="activenav" href="${section[0]}">${section[1]}</a></p>`
+    } else {
+        ih += `<p><a class="navitem" href="${section[0]}">${section[1]}</a></p>`
+    }
+    idx += 1
+}
+ih += "</nav>"
+document.getElementsByClassName('docsidebar')[0].innerHTML = ih
+if (sectionIdx > 0) {
+    document.getElementsByClassName('bottomnavbefore')[0].innerHTML = 
+        `Prev: <a href="${sections[sectionIdx - 1][0]}">${sections[sectionIdx - 1][1]}</a>`
+}
+if (sectionIdx < sections.length - 1) {
+    document.getElementsByClassName('bottomnavafter')[0].innerHTML = 
+        `Next: <a href="${sections[sectionIdx + 1][0]}">${sections[sectionIdx + 1][1]}</a>`
+}
+
+document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', `
+    <title>See Sailing: ${sections[sectionIdx][1]}</title>
+    <meta property="og:title" content="See Sailing: ${sections[sectionIdx][1]}" />
+    <meta property="og:description" content="Online documentation for See Sailing: ${sections[sectionIdx][1]}" />
+    <meta name="description" content="Online documentation for See Sailing: ${sections[sectionIdx][1]}" />
+`)
---
+++ header.js
@@ -0,0 +1,43 @@
+const headElement = document.getElementsByTagName('head')[0]
+const bodyElement = document.getElementsByTagName('body')[0]
+
+headElement.insertAdjacentHTML('beforeend', `
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="${window.location.href}" />
+`)
+
+bodyElement.insertAdjacentHTML('beforebegin', `
+<header>
+    <a href="/"><img id="logo" src="/logo-blue.svg" alt="See Sailing"></a>
+    <span class="spacer"></span>
+    <nav>
+        <a href="https://www.facebook.com/profile.php?id=61564439514836"><img class="fb" src="/fb.png" alt="See-Sailing on Facebook"></a>
+        <span class="navitem"><a class="navitem" href="/buy.html">BUY</a></span>
+        <span class="navitem"><a class="navitem" href="/docs/index.html">DOCS</a></span>
+        <span class="navitem"><a class="navitem" href="https://seesailing.substack.com">NEWS</a></span>
+    </nav>
+</header>
+`)
+
+bodyElement.insertAdjacentHTML('afterend', `
+<footer>
+    <p>More: <a href="/privacy.html">Privacy Policy</a> <a href="/partners.html">Partnering</a>
+    <span style="float: right;">Made in Wellington NZ, contact <a href="mailto:davep@see-sailing.com" style="color: #555;">David Preece</a>.</span></p>
+</footer>
+`)
+
+let foundImage = false
+for (element of document.getElementsByTagName('meta')) {
+    if (element.getAttribute('property') === 'og:image') {
+        foundImage = true
+    }
+    if (element.getAttribute('property') === 'og:description') {
+        headElement.insertAdjacentHTML('beforeend', `
+            <meta name="description" content="${element.getAttribute('content')}" />`)
+    }
+}
+if (!foundImage) {
+    headElement.insertAdjacentHTML('beforeend', `
+        <meta property="og:image" content="${window.location.origin}/social.png" />`)
+}
+
